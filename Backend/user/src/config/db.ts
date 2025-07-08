675dde09da0752ec7501f37dfd7d4aef20183e1e
Aayush <khadkaaayush@proton.me>
2025-07-08 18:56:21 +0545

    Projected Started and Database connected for user backend

---
+++ Backend/user/src/config/db.ts
@@ -0,0 +1,18 @@
+import mongoose, { Error } from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+const connectDb = async () => {
+  const url = process.env.MONGO_URI;
+  if (!url) throw new Error("MONGO URI IS NOT GIVEN");
+
+  try {
+    await mongoose.connect(url, { dbName: "Chattingapp" });
+    console.log("DATABASE CONNECTED!!");
+  } catch (error) {
+    console.error("failed to connect to MONGODB", error);
+    process.exit(1);
+  }
+};
+
+export default connectDb;
